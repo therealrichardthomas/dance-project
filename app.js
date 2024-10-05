c1d777c6e4d721dc39801ba9d2d246cee84f41a3
Richard Thomas <richuthomasp@gmail.com>
2024-10-05 17:18:21 -0400

    Add app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+const express = require('express'); // using express modules
+const path = require('path'); 
+const fs = require('fs'); // using a file system
+const app = express(); // importing express
+const port = 80; // creating a port to run my files
+
