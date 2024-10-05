f9cbe7467656a2283a879ce33ec22592eafb900e
Richard Thomas <richuthomasp@gmail.com>
2024-10-05 17:56:29 -0400

    Add express, file system and path modules. Set pug as template engineer.

--- app.js
+++ app.js
@@ -2,5 +2,23 @@ const express = require('express'); // using express modules
 const path = require('path'); 
 const fs = require('fs'); // using a file system
 const app = express(); // importing express
-const port = 80; // creating a port to run my files
+const port = 8000; // creating a port to run my files
 
+// EXPRESS SPECIFIC STUFF
+app.use("/static", express.static('static')); // used just for viewing not running the file
+app.use(express.urlencoded());
+
+// PUG SPECIFIC STUFF
+app.set('view engine', 'pug');// Set the template engineer as pug
+app.set('views', path.join(__dirname, 'views')) // 'views' is folder name //
+
+// ENDPOINTS
+app.get('/', (req, res) => {
+    const params = {};
+    res.status(200).render('index.pug', params); // file we want to run/render
+});
+
+// STARTING THE SERVER
+app.listen(port, () => {
+    console.log(`The application started successfully on port ${port}`);
+});
